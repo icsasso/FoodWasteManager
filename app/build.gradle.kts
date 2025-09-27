a0adf970353afc9647a8c5ee1c4db9faed9a8015
claudiacordero1904 <ccorderoramos@wm.edu>
2025-09-27 15:01:07 -0400

    Integrated the Mealdb api. Meal.kt: Creates the data class to enter the JSONified variables from the mealdb API in. MealResponse.kt: Creates a wrapper for the Meal.kt data class list MealApi.kt: Creates Get methods that allows users to search for a specific or random food RetrofitClient.kt: Uses retrofit to integrate the base URL for the api, creates a new RetrofitInstance, and adds a converter to convert the JSON data from the mealdb API into a Kotlin object

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
 }
 
+
+
 android {
     namespace = "com.example.foodwastemanager"
     compileSdk = 35
@@ -45,4 +47,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
